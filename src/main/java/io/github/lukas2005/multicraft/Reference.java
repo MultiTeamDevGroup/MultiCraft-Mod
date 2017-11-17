0e5be0b9a3c83d3f56c7274f6e79f8d87cb11536
lukas2005 <lukas2005.38@gmail.com>
2017-11-17 12:30:47 +0100

    changed version number

--- src/main/java/io/github/lukas2005/multicraft/Reference.java
+++ src/main/java/io/github/lukas2005/multicraft/Reference.java
@@ -4,7 +4,7 @@ public class Reference {
 
     public static final String MOD_ID = "multic";
     public static final String NAME = "MultiCraft";
-    public static final String VERSION = "0.0.1";
+    public static final String VERSION = "0.1.1";
 
     public static final String SERVER_PROXY = "io.github.lukas2005.multicraft.proxy.ServerProxy";
     public static final String CLIENT_PROXY = "io.github.lukas2005.multicraft.proxy.ClientProxy";
