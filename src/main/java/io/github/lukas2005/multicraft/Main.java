b980cdc903e11bd7120651ab60d73ac4909a9d33
MonstahGames <30873388+MonstahGames@users.noreply.github.com>
2017-11-19 21:20:17 +0100

    Logger info

--- src/main/java/io/github/lukas2005/multicraft/Main.java
+++ src/main/java/io/github/lukas2005/multicraft/Main.java
@@ -7,11 +7,17 @@ import net.minecraftforge.fml.common.SidedProxy;
 import net.minecraftforge.fml.common.event.FMLInitializationEvent;
 import net.minecraftforge.fml.common.event.FMLPostInitializationEvent;
 import net.minecraftforge.fml.common.event.FMLPreInitializationEvent;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 
 import java.util.Random;
 
 @Mod(modid = Reference.MOD_ID, name = Reference.NAME, version = Reference.VERSION)
 public class Main {
+
+    public static Logger LOGGER = LogManager.getLogger(Reference.MOD_ID.toUpperCase());
+
+    public static boolean DEBUG = true;
 
     @SidedProxy(serverSide = Reference.SERVER_PROXY, clientSide = Reference.CLIENT_PROXY)
     public static IProxy proxy;
@@ -20,18 +26,23 @@ public class Main {
 
     @EventHandler
     public static void preInit(FMLPreInitializationEvent e) {
+
         proxy.preInit(e);
     }
 
     @EventHandler
     public static void init(FMLInitializationEvent e) {
-
+        if (DEBUG) {
+            LOGGER.info("Loading Multicraft...");
+        }
         proxy.init(e);
     }
 
     @EventHandler
     public static void postInit(FMLPostInitializationEvent e) {
-
+        if (DEBUG) {
+            LOGGER.info("Multicraft Loaded!");
+        }
         proxy.postInit(e);
     }
 
