5dc0cd51892739a84d1e458e7da5f0e01d628cb4
lukas2005 <lukas2005.38@gmail.com>
2017-11-25 17:56:56 +0100

    little formatting thing

--- src/main/java/io/github/lukas2005/multicraft/armor/PortableFurnance.java
+++ src/main/java/io/github/lukas2005/multicraft/armor/PortableFurnance.java
@@ -21,7 +21,8 @@ public class PortableFurnance extends ArmorBase {
                 0,
                 0,
                 SoundEvents.ITEM_ARMOR_EQUIP_IRON,
-                true, false, true);
+                true,
+                false, true);
 
         replaceArmorPiece(EntityEquipmentSlot.CHEST, new PortableFurnancePiece());
     }
