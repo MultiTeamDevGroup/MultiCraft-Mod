b038db0cd09eb01b7e84fec2a150ed48c93230e1
lukas2005 <lukas2005.38@gmail.com>
2017-11-26 14:01:55 +0100

    hmmmm

--- src/main/java/io/github/lukas2005/multicraft/gui/PortableFurnaceGui.java
+++ src/main/java/io/github/lukas2005/multicraft/gui/PortableFurnaceGui.java
@@ -3,5 +3,18 @@ package io.github.lukas2005.multicraft.gui;
 import net.minecraft.client.gui.GuiScreen;
 
 public class PortableFurnaceGui extends GuiScreen {
-    
+
+    public PortableFurnaceGui() {
+
+    }
+
+    @Override
+    public void initGui() {
+    }
+
+    @Override
+    public void drawScreen(int mouseX, int mouseY, float partialTicks) {
+        this.drawDefaultBackground();
+        super.drawScreen(mouseX, mouseY, partialTicks);
+    }
 }
